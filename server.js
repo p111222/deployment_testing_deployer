d50e74805f69967707eb1aefde11604cb332baaa
p111222 <princejainpre.ram@gmail.com>
2024-06-07 13:34:11 +0530

    prince changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 6000;
 
 // Route to trigger execution of shell script
 app.get('/deploy/princedeployer/client', (req, res) => {
-    exec('sh ./scripts/client.sh', (err, stdout, stderr) => {
+    exec('sh ./Scripts/client.sh', (err, stdout, stderr) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ msg: 'Error executing script', error: err });
