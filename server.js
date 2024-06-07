4c7186096e4fa07851debec87b99c4523a7ae3c1
p111222 <princejainpre.ram@gmail.com>
2024-06-07 12:03:21 +0530

    prince changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const port = 6000;
+
+// Route to trigger execution of shell script
+app.get('/deploy/princedeployer/client', (req, res) => {
+    exec('sh ./scripts/client.sh', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ msg: 'Error executing script', error: err });
+        }
+        return res.status(200).send('Client deployed successfully');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
