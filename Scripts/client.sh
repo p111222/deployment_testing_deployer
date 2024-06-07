e2b98e29780a328b3c865b07d8c62215a80fa23e
p111222 <princejainpre.ram@gmail.com>
2024-06-07 12:09:51 +0530

    prince changes

---
+++ Scripts/client.sh
@@ -0,0 +1,27 @@
+
+# Change to your project directory
+cd /nsldev/prince_deployer/
+
+sudo rm -r ./deployment_testing
+
+# Pull latest changes
+sudo git clone https://p111222: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@github.com/p111222/deployment_testing.git
+
+# Build the client
+cd /nsldev/prince_deployer/deployment_testing/client
+sudo npm install
+sudo npm run build
+
+# Stop and remove the existing container
+docker stop deployment_testing_client
+docker rm deployment_testing_client
+
+# Remove existing image
+docker rmi deployment_testing_client
+
+# Build Docker image from Dockerfile in current directory
+cd /nsldev/prince_deployer/deployment_testing/client
+docker build -t deployment_testing_client .
+
+# Run Docker container
+docker run -d -p 4001:80 --name deployment_testing_client deployment_testing_client
